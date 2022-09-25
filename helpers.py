4fe51f02ccf698c991d7bcf9b9ee7334834f705f
Bekkelien <bekkelien@hotmail.com>
2022-09-25 13:55:05 +0200

    datapaser cleanup

--- helpers.py
+++ helpers.py
@@ -1,34 +1,35 @@
 import pyaudio  
 import pandas as pd
 
+def extract_metadata(file_path):
+    if 'LJSpeech-1.1' in str(file_path):
+        df_metadata = pd.read_csv(str(file_path), delimiter = '|', header=None)
+        df_metadata.columns = [str(x).replace(str(x),str(x)) for x in ['filenames','text','text2']]
+        return df_metadata
 
-def extract_metadata():
-    df_metadata = pd.read_csv('data/LJSpeech-1.1/metadata.csv', delimiter = '|', header=None)
-    df_metadata.columns = [str(x).replace(str(x),str(x)) for x in ['filenames','text','text2']]
-    return df_metadata
 
 def play_audio(f, chunk = 1024):
     """ Input a f from wave.open(file.wav,"rb")  """
 
-    #instantiate PyAudio  
+    # Instantiate PyAudio  
     p = pyaudio.PyAudio()  
 
-    #open stream  
+    # Open stream  
     stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
                                                     channels = f.getnchannels(),  
                                                     rate = f.getframerate(),  
                                                     output = True)  
-    #read data  
+    # Read data  
     data = f.readframes(chunk)  
 
-    #play stream  
+    # Play stream  
     while data:  
         stream.write(data)  
         data = f.readframes(chunk)  
         
-    #stop stream  
+    # Stop stream  
     stream.stop_stream()  
     stream.close()  
 
-    #close PyAudio  
+    # Close PyAudio  
     p.terminate()
